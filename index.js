c7023428aab34b737f12492565d82b8543318e53
nkpe <naomi.pearse@gmail.com>
2022-08-15 10:15:00 +0100

    Number counter made

---
+++ index.js
@@ -0,0 +1,12 @@
+// Multiples of 3 = Fizz
+// Multiples of 5 = Buzz
+// Multiples of both = FizzBuzz
+
+let numCounter = () => {
+    let i;
+    for (i=1; i<101 ; i++) {
+    console.log(i);
+    }
+};
+
+numCounter();
