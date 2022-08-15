d05cf9f4b516948663c2b861cce15948456a786e
nkpe <naomi.pearse@gmail.com>
2022-08-15 11:46:02 +0100

    Multiple check start

--- index.js
+++ index.js
@@ -5,7 +5,20 @@
 let numCounter = () => {
     let i;
     for (i=1; i<101 ; i++) {
-    console.log(i);
+        isMultiple(i);
+    };
+    
+};
+
+let isMultiple = (i) => {
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)){
+        console.log("FizzBuzz");
+    } else if (Number.isInteger(i/3)){
+        console.log("Fizz");
+    } else if (Number.isInteger){
+        console.log("Buzz");
+    } else {
+        console.log(i);
     }
 };
 
