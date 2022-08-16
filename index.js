b2d35c8953e91a318de792c9ee1d4ce3f1a3b86c
nkpe <naomi.pearse@gmail.com>
2022-08-16 09:31:13 +0100

    Completed to console

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let isMultiple = (i) => {
         console.log("FizzBuzz");
     } else if (Number.isInteger(i/3)){
         console.log("Fizz");
-    } else if (Number.isInteger){
+    } else if (Number.isInteger(i/5)){
         console.log("Buzz");
     } else {
         console.log(i);
