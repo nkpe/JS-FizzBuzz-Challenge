91c0e1beb31229c2fc675d165fba525a07b16645
nkpe <naomi.pearse@gmail.com>
2022-08-16 09:42:56 +0100

    Output to Page

--- index.js
+++ index.js
@@ -2,24 +2,38 @@
 // Multiples of 5 = Buzz
 // Multiples of both = FizzBuzz
 
+const Body = document.getElementById("main-body");
+let content;
+
+let isMultiple = (i) => {
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)){
+        content = "FizzBuzz";
+    } else if (Number.isInteger(i/3)){
+       content = "Fizz";
+    } else if (Number.isInteger(i/5)){
+        content = "Buzz";
+    } else {
+        content = i;
+    }
+};
+
+let printToPage = () => {
+    let p = document.createElement("p");
+    console.log(content);
+    p.textContent = content;
+    Body.appendChild(p);
+}
+
+
 let numCounter = () => {
     let i;
     for (i=1; i<101 ; i++) {
         isMultiple(i);
+        printToPage();
     };
     
 };
 
-let isMultiple = (i) => {
-    if (Number.isInteger(i/3) && Number.isInteger(i/5)){
-        console.log("FizzBuzz");
-    } else if (Number.isInteger(i/3)){
-        console.log("Fizz");
-    } else if (Number.isInteger(i/5)){
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-};
-
 numCounter();
+
+
