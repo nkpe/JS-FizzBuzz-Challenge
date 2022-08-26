17b26b346231439df74ed6794179b0bf4ac2f6f8
nkpe <naomi.pearse@gmail.com>
2022-08-26 17:50:18 +0100

    values set - x,y, speed etc

--- bouncingBalls/src/scripts/index.js
+++ bouncingBalls/src/scripts/index.js
@@ -2,12 +2,13 @@ console.log('JS file working');
 
 // TODO: Set up how many balls we want
 const mainEl = document.getElementById('app');
+const footerEl = document.getElementById('footer');
 let width = window.innerWidth;
 let height = window.innerHeight;
 
 let windowSize = () => {
     let pEl = document.createElement('p');
-    mainEl.appendChild(pEl);
+    footerEl.appendChild(pEl);
     pEl.innerText = `${width}px + ${height}px`;
 };
 
@@ -25,82 +26,76 @@ let r = [];
 let g = [];
 let b = [];
 
-let random = (min, max) => {
+let randomNegInt = (min, max) => {
     min = Math.ceil(min);
-    console.log(min);
     max = Math.floor(max);
-    console.log(max);
-    return Math.floor(Math.random() * (max - min) + max);
-}
+    return Math.floor(Math.random() * (min - max) + max);
+};
 
-let createCanvas = () => {
+let randomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+let createCanvas = (width) => {
     const canvasEl = document.createElement("canvas");
     mainEl.appendChild(canvasEl);
-    canvasEl.style.height = height;
+    canvasEl.style.height = "100%";
     canvasEl.style.width = width;
+    canvasEl.style.backgroundColor = "black";
 };
 
 let setUp = () => {
-
+    createCanvas(width);
     // TODO: Create a for loop that iterates through i until it reaches the ball count value
     // Inside the for loop:
     // TODO: Set x and y position to be the center
     // TODO: Set the speeds to be random
     // TODO: Set the size to be random
     // TODO: Set the colors to be random
-    for (let i = 0; i <= ballCount; i++) {
+    for (let i = 0; i <ballCount; i++) {
         x[i] = width / 2;
         y[i] = height / 2;
-        xSpeed[i] = random(-5, 5);
-        ySpeed[i] = random(-5, 5);
+        xSpeed[i] = randomNegInt(-5, 5);
+        ySpeed[i] = randomNegInt(-5, 5);
+        size[i] = randomInt(1, 10);
+        r[i] = randomInt(0, 255);
+        g[i] = randomInt(0, 255);
+        b[i] = randomInt(0, 255);
     }
-    console.log(`x , y , xSpeed , ySpeed`, x, y, xSpeed, ySpeed);
 
-
+    console.log("ySpeed=" , ySpeed);
 }
 
 windowSize();
-createCanvas();
 setUp();
 
-
-
-
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-
-
-
-
-
-}
-
-function draw() {
-    background(0, 50);
+let draw = () => {
+    
 
     // TODO: Iterate through a new for loop to change the properties in order to animate the balls
+        // Inside the for loop:
+            // TODO: Increment speed for x position
+            // TODO: Increment speed for y position
+            // TODO: Reverse x direction if ball hits left or right sides
+            // TODO: Reverse y direction if ball hits top or bottom sides
+            // TODO: Set random R, G, B values
+            // TODO: Style to have no strokes
+            // TODO: Draw the bouncing balls
+    for (let i=0; i<ballCount; i++){
+        xSpeed[i] = xSpeed++;
+        ySpeed[i] = ySpeed++;
+        if(x===0 || x===width){
+            x[i] = x--;
+        } else if (y===0 || y===height){
+            y[i] = y--;
+        } else {
+            return;
+        };
 
-    // Inside the for loop:
-    // TODO: Increment speed for x position
-
-    // TODO: Increment speed for y position
-
-
-    // TODO: Reverse x direction if ball hits left or right sides
-
-
-    // TODO: Reverse y direction if ball hits top or bottom sides
-
-
-    // TODO: Set random R, G, B values
-
-
-    // TODO: Style to have no strokes
-
-
-    // TODO: Draw the bouncing balls
-
+        
+    }
 
 }
 
